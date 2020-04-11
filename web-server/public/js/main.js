06a5e594d18a5ec89c0ac7530657aa68c7491e36
ithar <itharm@ext.inditex.com>
2020-04-11 12:14:28 +0100

    Added jquery and form submit logic

--- web-server/public/js/main.js
+++ web-server/public/js/main.js
@@ -1,1 +1,30 @@
 console.log('client side JS')
+
+$( document ).ready(function() {
+    
+    // Ajax
+    function perforSearch(address) {
+        
+        const url = 'http://localhost:3000/weather?address=' + address
+
+        fetch(url).then((response) => {
+            response.json().then((data) => {
+                console.log(data)
+            })
+        })
+    }
+
+    // Events
+    $('#searchForm').submit((e) => {
+        e.preventDefault()
+        const address = $('#address').val()
+        perforSearch(address)
+    })
+
+    $('#searchBtn').click(() => {
+        const address = $('#address').val()
+        perforSearch(address)
+    })
+  
+
+});
